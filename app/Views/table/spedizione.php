86b353daa8a560faaa9ff7e7086aceb49d72ba8a
Vladimir <zabrodkin.vladimir@gmail.com>
2025-05-20 14:14:48 +0300

    wip

--- app/Views/table/spedizione.php
+++ app/Views/table/spedizione.php
@@ -1,22 +1,25 @@
-<script>
-  const salesData = <?= json_encode($result ?? [], JSON_UNESCAPED_UNICODE) ?>;
-  const headers = salesData[0];
-  const rows = salesData;
+ <div id="data-table">
+   <input type="text" id="data-table-input">
+ </div>
+ <script>
+   const salesData = <?= json_encode($result ?? [], JSON_UNESCAPED_UNICODE) ?>;
+   const headers = salesData[0];
+   const rows = salesData;
 
-  const div = document.getElementById("data-table");
-  const inputElement = document.getElementById("data-table-input");
+   const div = document.getElementById("data-table");
+   const inputElement = document.getElementById("data-table-input");
 
-  const table = spreadsheet.createTable({
-    rows: rows.length,
-    columns: headers.length,
-    textInputElement: inputElement,
-    headers,
-    filterTypes: [],
-    data: rows,
-    fixedRows: 1,
-    fixedCols: 2,
-    computedColumns: [4, 5, 6],
-  });
+   const table = spreadsheet.createTable({
+     rows: rows.length,
+     columns: headers.length,
+     textInputElement: inputElement,
+     headers,
+     filterTypes: [],
+     data: rows,
+     fixedRows: 1,
+     fixedCols: 2,
+     computedColumns: [4, 5, 6],
+   });
 
-  div.appendChild(table.element);
-</script>
+   div.appendChild(table.element);
+ </script>
