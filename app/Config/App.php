ddd0d5fdd34f7f52bdba4557d789d555daca493e
Vladimir <zabrodkin.vladimir@gmail.com>
2025-05-30 09:29:30 +0300

    fix()

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = '';
+    public string $baseURL = 'https://clothis.kebeta.agency/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
